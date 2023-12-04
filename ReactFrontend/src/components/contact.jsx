1dcd8898331fd8e80338103032592e7eb1a3b247
TedH04 <105521543+TedH04@users.noreply.github.com>
2023-12-04 11:29:30 +0100

    cntact page, individuals and companies

--- ReactFrontend/src/components/contact.jsx
+++ ReactFrontend/src/components/contact.jsx
@@ -3,91 +3,180 @@ import React from "react";
 import "./styling/contact.css";
 
 const initialState = {
+  customerType: "individual", // Initial customer type
   name: "",
+  address: "",
+  city: "",
+  postalCode: "",
   email: "",
-  message: "",
+  description: "",
+  companyName: "",
+  orgNumber: "",
 };
+
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
+  const [state, setState] = useState(initialState);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setState((prevState) => ({ ...prevState, [name]: value }));
   };
 
+  const handleCustomerTypeChange = (type) => {
+    setState({ ...initialState, customerType: type });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name, email, message);
+    console.log(state);
   };
+  const renderFormFields = () => {
+    // Common fields
+    const fields = (
+      <>
+        <div className="form-group">
+          <input
+            type="text"
+            name="name"
+            className="form-control"
+            placeholder="För & Efternamn"
+            value={state.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="address"
+            className="form-control"
+            placeholder="Adress"
+            value={state.address}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="city"
+            className="form-control"
+            placeholder="Stad"
+            value={state.city}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="postalCode"
+            className="form-control"
+            placeholder="Postnummer"
+            value={state.postalCode}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            placeholder="Email"
+            value={state.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="description"
+            className="form-control"
+            rows="4"
+            placeholder="Beskriv vad ni behöver hjälp med"
+            value={state.description}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+      </>
+    );
+
+    // Additional fields for companies
+    if (state.customerType === "company") {
+      return (
+        <>
+          {fields}
+          <div className="form-group">
+            <input
+              type="text"
+              name="companyName"
+              className="form-control"
+              placeholder="Företags Namn"
+              value={state.companyName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="orgNumber"
+              className="form-control"
+              placeholder="Organisations Nummer"
+              value={state.orgNumber}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </>
+      );
+    }
+
+    return fields;
+  };
+
   return (
     <div>
       <div id="contact">
         <div className="container">
           <div className="col-md-8">
-            <div className="row">
-              <div className="section-title">
-                <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
-              </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Name"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success"></div>
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
-                </button>
-              </form>
+            <div className="section-title">
+              <h2>Skicka jobbförfrågan</h2>
+              <p>
+              För att skicka in er jobbförfrågan, vänligen fyll i era uppgifter i formuläret nedan.
+              </p>
             </div>
+            <div className="customer-type-buttons">
+              <button
+                type="button"
+                className={`btn ${state.customerType === "individual" ? "btn-primary" : "btn-default"}`}
+                onClick={() => handleCustomerTypeChange("individual")}
+              >
+                Privatperson
+              </button>
+              <button
+                type="button"
+                className={`btn ${state.customerType === "company" ? "btn-primary" : "btn-default"}`}
+                onClick={() => handleCustomerTypeChange("company")}
+              >
+                Företag
+              </button>
+            </div>
+            <form name="sentMessage" validate onSubmit={handleSubmit}>
+              {renderFormFields()}
+              <button type="submit" className="btn btn-custom btn-lg">
+                Skicka Förfrågan
+              </button>
+            </form>
           </div>
           <div className="col-md-3 col-md-offset-1 contact-info">
             <div className="contact-item">
               <h3>Contact Info</h3>
               <p>
                 <span>
-                  <i className="fa fa-map-marker"></i> Address
+                  <i className="fa fa-map-marker"></i> Adress: 
                 </span>
                 {props.data ? props.data.address : "loading"}
               </p>
@@ -95,7 +184,7 @@ export const Contact = (props) => {
             <div className="contact-item">
               <p>
                 <span>
-                  <i className="fa fa-phone"></i> Phone
+                  <i className="fa fa-phone"></i> Nummer: 
                 </span>{" "}
                 {props.data ? props.data.phone : "loading"}
               </p>
@@ -103,7 +192,7 @@ export const Contact = (props) => {
             <div className="contact-item">
               <p>
                 <span>
-                  <i className="fa fa-envelope-o"></i> Email
+                  <i className="fa fa-envelope-o"></i> Email: 
                 </span>{" "}
                 {props.data ? props.data.email : "loading"}
               </p>
--- ReactFrontend/src/components/gallery.jsx
+++ ReactFrontend/src/components/gallery.jsx
@@ -8,8 +8,7 @@ export const Gallery = (props) => {
         <div className="section-title">
           <h2>Gallery</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Se några av våra tidigare project här
           </p>
         </div>
         <div className="row">
